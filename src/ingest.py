f694d23c6413c34a4f54f4144146261618a7202d
Clayton Rafael <clayton.rssouza@gmail.com>
2026-01-12 21:16:46 -0300

    feat: add ingest function

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,28 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+if not PDF_PATH:
+    raise ValueError("PDF_PATH is not set")
+
+# loader = PyPDFLoader(PDF_PATH)
+# documents = loader.load()
+
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+# chunks = text_splitter.split_documents(documents)
 
 def ingest_pdf():
-    pass
+    loader = PyPDFLoader(PDF_PATH)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(documents)
+    return chunks
 
 
 if __name__ == "__main__":
-    ingest_pdf()
+    chunks = ingest_pdf()
+    print(len(chunks))
