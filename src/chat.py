f64cc5701b5a5a79a1493122dcf90fd075270f28
Caio Maia <caiomaia3@gmail.com>
2026-04-06 20:50:22 -0300

    feat(chatbot): chatbot funcional

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,24 @@
+from langchain_openai import ChatOpenAI
+
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+    question = "Qual a capital da França" 
+    chain = search_prompt(question)
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
-    
-    pass
+
+    GPT_NANO = "gpt-5-nano"
+    llm = ChatOpenAI(model=GPT_NANO, temperature=0)
+
+    new_chain = chain | llm
+
+    result = new_chain.invoke({"pergunta":question})
+
+    print(f"Pergunta: {question}")
+    print(f"Resposta: {result.content}")
 
 if __name__ == "__main__":
     main()
